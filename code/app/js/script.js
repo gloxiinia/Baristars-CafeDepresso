ed15990b6c3b6c8844159647f5a4d259243cb1a5
gloxiinia <91533469+gloxiinia@users.noreply.github.com>
2021-12-14 00:03:49 +0700

    updated the landing page code

--- code/app/js/script.js
+++ code/app/js/script.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+const myFunction = () => {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
